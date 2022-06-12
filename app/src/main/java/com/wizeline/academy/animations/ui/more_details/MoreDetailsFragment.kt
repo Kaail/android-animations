7b37966ad4c271e0601db6244499b7827201b83d
Saul Emmanuel Flores Bernal <saul.fb@wizeline.com>
2022-06-12 16:12:59 -0500

    Feature: Add image zoom animation when pinching MoreDetailsFragment's ImageView

--- app/src/main/java/com/wizeline/academy/animations/ui/more_details/MoreDetailsFragment.kt
+++ app/src/main/java/com/wizeline/academy/animations/ui/more_details/MoreDetailsFragment.kt
@@ -1,11 +1,17 @@
 package com.wizeline.academy.animations.ui.more_details
 
 import android.R as AndroidR
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.transition.TransitionInflater
 import android.view.LayoutInflater
+import android.view.MotionEvent
+import android.view.ScaleGestureDetector
 import android.view.View
 import android.view.ViewGroup
+import androidx.dynamicanimation.animation.DynamicAnimation
+import androidx.dynamicanimation.animation.SpringAnimation
+import androidx.dynamicanimation.animation.SpringForce
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
@@ -20,6 +26,61 @@ class MoreDetailsFragment : Fragment() {
     private val binding get() = _binding!!
     private val viewModel: MoreDetailsViewModel by viewModels()
     private val args: MoreDetailsFragmentArgs by navArgs()
+
+    private lateinit var scaleXAnimation: SpringAnimation
+    private lateinit var scaleYAnimation: SpringAnimation
+    private lateinit var scaleGestureDetector: ScaleGestureDetector
+
+    private companion object Params {
+        const val INITIAL_SCALE = 1f
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun setupScaleAnimation() {
+        scaleXAnimation = createSpringAnimation(binding.ivImageDetailLarge, SpringAnimation.SCALE_X)
+
+        scaleYAnimation = createSpringAnimation(binding.ivImageDetailLarge, SpringAnimation.SCALE_Y)
+
+        setupPinchToZoom()
+
+        binding.ivImageDetailLarge.setOnTouchListener { _, event ->
+            if (event.action == MotionEvent.ACTION_UP) {
+                scaleXAnimation.start()
+                scaleYAnimation.start()
+            } else {
+                scaleXAnimation.cancel()
+                scaleYAnimation.cancel()
+                scaleGestureDetector.onTouchEvent(event)
+            }
+            true
+        }
+    }
+
+    private fun setupPinchToZoom() {
+        var scaleFactor = INITIAL_SCALE
+        scaleGestureDetector = ScaleGestureDetector(
+            requireContext(),
+            object : ScaleGestureDetector.SimpleOnScaleGestureListener() {
+                override fun onScale(detector: ScaleGestureDetector): Boolean {
+                    scaleFactor *= detector.scaleFactor
+                    binding.ivImageDetailLarge.scaleX *= scaleFactor
+                    binding.ivImageDetailLarge.scaleY *= scaleFactor
+                    return true
+                }
+            }
+        )
+    }
+
+    private fun createSpringAnimation(
+        view: View, property: DynamicAnimation.ViewProperty
+    ): SpringAnimation {
+        val animation = SpringAnimation(view, property)
+        val spring = SpringForce(INITIAL_SCALE)
+        spring.stiffness = SpringForce.STIFFNESS_HIGH
+        spring.dampingRatio = SpringForce.DAMPING_RATIO_NO_BOUNCY
+        animation.spring = spring
+        return animation
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -39,5 +100,7 @@ class MoreDetailsFragment : Fragment() {
 
         sharedElementEnterTransition = TransitionInflater.from(requireContext())
             .inflateTransition(AndroidR.transition.move)
+
+        setupScaleAnimation()
     }
 }
